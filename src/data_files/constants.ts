342c3d1aefdec5b6b6e1b78651052d008875a4df
snehatumminakatti1 <snehatumminakatti1@gmail.com>
2025-01-27 19:15:07 +0530

    added keywords

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,8 +3,8 @@ import ogImageSrc from "@images/social.png";
 export const SITE = {
   title: "SamarthMeditech",
   tagline: "Transforming Healthcare with Smart Solutions",
-  description: "Samarth Meditech is a leading provider of innovative medical technology solutions that simplify business management and drive growth for healthcare professionals. Our cutting-edge applications leverage advanced technology to streamline operations, enhance patient care, and boost profitability. With a commitment to local support and personalized service, Samarth Meditech partners with industry experts to deliver responsive solutions tailored to the unique needs of each client. Experience the future of medical technology with Samarth Meditech today.Pharmacy Management",
-  description_short: "Samarth Meditech delivers advanced technology solutions for healthcare and businesses, specializing in custom software, patient management systems, and mobile applications to enhance efficiency and performance.",
+  description: " Best EHR EMR Software by Samarth Meditech|  Samarth Meditech is a leading provider of innovative medical technology solutions that simplify business management and drive growth for healthcare professionals. Our cutting-edge applications leverage advanced technology to streamline operations, enhance patient care, and boost profitability. With a commitment to local support and personalized service, Samarth Meditech partners with industry experts to deliver responsive solutions tailored to the unique needs of each client. Experience the future of medical technology with Samarth Meditech today.Pharmacy Management",
+  description_short: "Medical Software Management in Hubli-Dharwad | Clinic Management Sofware | EMR Software | Samarth Meditech delivers advanced technology solutions for healthcare and businesses, specializing in custom software, patient management systems, and mobile applications to enhance efficiency and performance | Software Company near me ",
   url: "https://samarthmeditech.com",
   author: "Emil Gulamov",
 };
