b76ab7d60f91beb47d7f7cc6661f460aea8cd301
snehatumminakatti1 <snehatumminakatti1@gmail.com>
2025-01-29 12:04:39 +0530

    added certificates

--- src/data_files/constantsAbout.ts
+++ src/data_files/constantsAbout.ts
@@ -1,9 +1,9 @@
 import ogImageSrc from "@images/social.png";
 
 export const SITE = {
-  title: "SamarthMeditech",
-  tagline: "Transforming Healthcare with Smart Solutions",
-  description: " Best EHR EMR Software Hubli Dharwad,Samarth Meditech is a leading provider of innovative medical technology solutions that simplify business management and drive growth for healthcare professionals. Our cutting-edge applications leverage advanced technology to streamline operations, enhance patient care, and boost profitability. With a commitment to local support and personalized service, Samarth Meditech partners with industry experts to deliver responsive solutions tailored to the unique needs of each client. Experience the future of medical technology with Samarth Meditech today.Pharmacy Management",
+  title: "EHR systems, electronic patient records, EMR software",
+  tagline: "AI in healthcare,Medical Software Company",
+  description: " Best EHR EMR Software Hubli Dharwad,Best hospital software in India,Medical software for clinics,Medical data analytics,Healthcare automation,Software for hospitals",
   description_short: "Medical Software Management in Hubli-Dharwad , Clinic Management Sofware ,EMR Software , Samarth Meditech delivers advanced technology solutions for healthcare and businesses, specializing in custom software, patient management systems, and mobile applications to enhance efficiency and performance | Software Company near me ",
   url: "https://samarthmeditech.com",
   author: "Emil Gulamov",
@@ -33,7 +33,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Best Medical Software company in Hubli Dharwad`,
+  title: `${SITE.title}: : Advanced Tech for Healthcare`,
   description: "Samarth Meditech provides innovative healthcare and business technology solutions, offering custom software development, patient management systems, and mobile applications to streamline operations and improve efficiency. With a focus on cutting-edge technology, user-friendly interfaces, and scalable solutions, Samarth Meditech empowers businesses and healthcare providers to achieve operational excellence and drive growth.",
   image: ogImageSrc,
 };
